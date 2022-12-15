8d6688e9100fe8d5ca5ada58c78e0167363b2579
AlexTraveylan <monsieurdemares@gmail.com>
2022-12-15 11:36:52 +0100

    fix

--- back/src/main/java/com/jeniti/back/controller/ChannelController.java
+++ back/src/main/java/com/jeniti/back/controller/ChannelController.java
@@ -4,12 +4,10 @@ package com.jeniti.back.controller;
 import com.jeniti.back.entity.Channel;
 import com.jeniti.back.service.ChannelService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/channels")
 public class ChannelController {
 
     @Autowired
