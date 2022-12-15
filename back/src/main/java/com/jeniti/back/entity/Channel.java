bcdf991cc0362c4241d801cce07855dc0fbc58ee
fauch47 <nicolas.de_m@hotmail.fr>
2022-12-15 11:20:24 +0100

    add channel entity

--- back/src/main/java/com/jeniti/back/entity/Channel.java
+++ back/src/main/java/com/jeniti/back/entity/Channel.java
@@ -1,4 +1,28 @@
 package com.jeniti.back.entity;
 
+import lombok.Data;
+import lombok.NonNull;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
 public class Channel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created_at;
+
+    @NonNull
+    private String description;
+
+    @NonNull
+    private String name;
+
 }
