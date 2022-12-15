5a63dde6fe7db892f7d3f09f8a139195af858344
Jean Lalanne Desktop <jexn.lxlxnne@gmail.com>
2022-12-15 13:41:59 +0100

    add branch User

---
+++ back/src/main/java/com/jeniti/back/entity/User_class.java
@@ -0,0 +1,32 @@
+package com.jeniti.back.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User_class {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String username;
+
+    @ManyToOne
+    @JoinColumn(name = "current_channel_id")
+    @Column(nullable = false)
+    private Channel current_channel;
+}
