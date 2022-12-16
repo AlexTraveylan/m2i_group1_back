efc3c4f6a23f8c3f483447bd78e55593589c84d5
AlexTraveylan <monsieurdemares@gmail.com>
2022-12-16 16:05:21 +0100

    add ResponseEntity on channel

--- back/src/main/java/com/jeniti/back/controller/ChannelController.java
+++ back/src/main/java/com/jeniti/back/controller/ChannelController.java
@@ -3,6 +3,7 @@ package com.jeniti.back.controller;
 import com.jeniti.back.entity.Channel;
 import com.jeniti.back.service.ChannelService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -24,10 +25,19 @@ public class ChannelController {
         return cService.createChannel(channel);
     }
     @DeleteMapping("/{id}")
-    public void deleteChannel (@PathVariable long id) {
+    public ResponseEntity deleteChannel (@PathVariable long id) {
         if (id != 1) {
-            cService.deleteChannel(id);}
+            Optional<Channel> channel = cService.getByIdChannel(id);
+            if (channel.isPresent()) {
+                cService.deleteChannel(id);
+                return ResponseEntity.ok("Le channel n°" + id + " a bien été supprimé");
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } else {
+            return ResponseEntity.badRequest().build();
         }
+    }
 
 
     @GetMapping("/{id}")
@@ -35,28 +45,32 @@ public class ChannelController {
     {return cService.getByIdChannel(id);}
 
     @PutMapping("/{id}")
-    public Channel updateChannel(@PathVariable Long id, @RequestBody Channel c) {
+    public ResponseEntity<Channel> updateChannel(@PathVariable Long id, @RequestBody Channel c) {
         Optional<Channel> channel = cService.getByIdChannel(id);
-        if(channel.isPresent() && 1!=id) {
-            Channel mChannel = channel.get();
-            String cDescription = c.getDescription();
-            String cName = c.getName();
+        if(channel.isPresent()) {
+            if (1!=id) {
+                Channel mChannel = channel.get();
+                String cDescription = c.getDescription();
+                String cName = c.getName();
 
-            if (null != cDescription) {
-                mChannel.setDescription(cDescription);
-            }else {
-                System.out.println("updateChannel description null");
+                if (null != cDescription) {
+                    mChannel.setDescription(cDescription);
+                } else {
+                    System.out.println("updateChannel description null");
+                }
+
+                if (null != cName) {
+                    mChannel.setName(cName);
+                } else {
+                    System.out.println("updateChannel Name null");
+                }
+
+                return ResponseEntity.ok(cService.updateChannel(mChannel));
+            } else {
+                return ResponseEntity.badRequest().build();
             }
-
-            if (null != cName) {
-                mChannel.setName(cName);
-            }else {
-                System.out.println("updateChannel Name null");
-            }
-
-            return cService.updateChannel(mChannel);
-        }else {
-            return null;
+        } else {
+            return ResponseEntity.notFound().build();
         }
 
     }
