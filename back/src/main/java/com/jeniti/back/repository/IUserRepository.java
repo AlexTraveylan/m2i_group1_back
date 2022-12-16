eb8256251192967ec5dd7f78f8f4b67a688dfb4f
Jean Lalanne Desktop <jexn.lxlxnne@gmail.com>
2022-12-16 15:54:33 +0100

    delete channel

--- back/src/main/java/com/jeniti/back/repository/IUserRepository.java
+++ back/src/main/java/com/jeniti/back/repository/IUserRepository.java
@@ -1,12 +1,24 @@
 package com.jeniti.back.repository;
 
+import com.jeniti.back.entity.Channel;
 import com.jeniti.back.entity.User_class;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 public interface IUserRepository extends JpaRepository<User_class, Long> {
 
     Optional<User_class> findByEmail(String email);
+
+    @Transactional
+    @Modifying
+    @Query("update User_class u SET u.current_channel = :g WHERE u.current_channel = :c")
+    void setChannelGeneral(Channel c, Channel g);
+
+
+
 
 }
