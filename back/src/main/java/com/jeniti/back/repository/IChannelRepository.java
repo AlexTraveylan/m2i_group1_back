7d767992473b39285d9d99bd31127559ca7dfa43
Jean Lalanne Desktop <jexn.lxlxnne@gmail.com>
2022-12-16 10:45:52 +0100

    Tim est content

--- back/src/main/java/com/jeniti/back/repository/IChannelRepository.java
+++ back/src/main/java/com/jeniti/back/repository/IChannelRepository.java
@@ -2,6 +2,7 @@ package com.jeniti.back.repository;
 
 import com.jeniti.back.entity.Channel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface IChannelRepository extends JpaRepository<Channel, Long> {
 }
